689049c339da08f66ebde1a0e3558d06ef4673c0
Your Name <you@example.com>
2023-10-31 19:55:55 +0100

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import streamlit as st
 from flask import Flask, render_template
-from streamlit import caching
 
 app = Flask(__name__)
 
@@ -8,28 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-def main():
-    st.title("Streamlit-Flask Integration")
-    st.write("Dies ist eine Streamlit-Anwendung, die eine Flask-Route anzeigt.")
-    st.write("Besuche die Flask-Route auf /flask-route")
-
-    # Embed Flask route in Streamlit
-    with st.echo():
-        from streamlit import caching
-        caching.clear_cache()
-        from flask import Flask
-        app = Flask(__name__)
-
-        @app.route('/flask-route')
-        def flask_route():
-            return "Das ist die Flask-Route"
-
-        if __name__ == '__main__':
-            app.run(port=8501)
-
 if __name__ == '__main__':
-    main()
-
+    app.run()
 
 
 
